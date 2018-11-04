9a11d12e829df28456af1a59a590793257f73621
reve <kgw1988@naver.com>
2018-11-04 18:19:12 +0900

    first commit

---
+++ src/main/java/kr/revelope/demo/spring/cloud/confserv/ConfservApplication.java
@@ -0,0 +1,11 @@
+package kr.revelope.demo.spring.cloud.confserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfservApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfservApplication.class, args);
+    }
+}
---
+++ src/test/java/kr/revelope/demo/spring/cloud/confserv/ConfservApplicationTests.java
@@ -0,0 +1,14 @@
+package kr.revelope.demo.spring.cloud.confserv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfservApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
