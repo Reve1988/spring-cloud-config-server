16c7522aa3ad206d6d9625454a817214d1203c94
REVE <kgw1988@naver.com>
2018-11-04 21:17:28 +0900

    add @EnableConfigServer on ConfservApplication.class

--- src/main/java/kr/revelope/demo/spring/cloud/confserv/ConfservApplication.java
+++ src/main/java/kr/revelope/demo/spring/cloud/confserv/ConfservApplication.java
@@ -2,10 +2,12 @@ package kr.revelope.demo.spring.cloud.confserv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfservApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ConfservApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ConfservApplication.class, args);
+	}
 }
