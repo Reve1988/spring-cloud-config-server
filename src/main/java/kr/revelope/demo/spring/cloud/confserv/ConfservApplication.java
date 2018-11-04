aa06d2913c3240a3c5fbb7dd79c6be68f26951e3
REVE <kgw1988@naver.com>
2018-11-04 21:22:18 +0900

    add spring cloud config server document by log

--- src/main/java/kr/revelope/demo/spring/cloud/confserv/ConfservApplication.java
+++ src/main/java/kr/revelope/demo/spring/cloud/confserv/ConfservApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * http://cloud.spring.io/spring-cloud-config/2.0.x/multi/multi__spring_cloud_config_server.html
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfservApplication {
